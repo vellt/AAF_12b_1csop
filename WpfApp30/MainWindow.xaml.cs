b271fac97f8f43e117cff29be99012ae8dd580d3
vellt <szantobenjamin97@gmail.com>
2024-12-21 11:31:36 +0100

    mai ora

---
+++ WpfApp30/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp30
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int gep = new Random().Next(1, 11);
+        public MainWindow()
+        {
+            InitializeComponent();
+            csuszka.ValueChanged += Csuszka_ValueChanged;
+            gomb.Click += Gomb_Click;
+        }
+
+        private void Gomb_Click(object sender, RoutedEventArgs e)
+        {
+            int enTippem = (int)csuszka.Value;
+            if (gep==enTippem)
+            {
+                MessageBox.Show("Eltaláltad");
+            }
+            else if (gep>enTippem)
+            {
+                MessageBox.Show("Nagyobbra gondoltam");
+            }
+            else
+            {
+                MessageBox.Show("Kisebbre gondoltam");
+            }
+        }
+
+        private void Csuszka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            szam.Text = csuszka.Value.ToString();
+        }
+    }
+}
