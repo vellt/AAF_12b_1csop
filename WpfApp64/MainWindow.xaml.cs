a9aadfe33f375945a2e9053be6004841f07dd6cd
vellt <szantobenjamin97@gmail.com>
2025-01-08 21:04:53 +0100

    mai ora

---
+++ WpfApp64/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp64
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pirosCsuszka.ValueChanged += PirosCsuszka_ValueChanged;
+            zoldCsuszka.ValueChanged += ZoldCsuszka_ValueChanged;
+            kekCsuszka.ValueChanged += KekCsuszka_ValueChanged;
+        }
+
+        private void KekCsuszka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            kekErtek.Text = kekCsuszka.Value.ToString();
+            SzinezdBe();
+        }
+
+        private void ZoldCsuszka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            zoldErtek.Text = zoldCsuszka.Value.ToString();
+            SzinezdBe();
+        }
+
+        private void PirosCsuszka_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            pirosErtek.Text = pirosCsuszka.Value.ToString();
+            SzinezdBe();
+        }
+
+        private void SzinezdBe()
+        {
+            byte r =Convert.ToByte(pirosCsuszka.Value);
+            byte g =Convert.ToByte(zoldCsuszka.Value);
+            byte b =Convert.ToByte(kekCsuszka.Value);
+            kijelzo.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+    }
+}
